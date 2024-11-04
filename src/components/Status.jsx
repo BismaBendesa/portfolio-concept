6d58449fd1839ab091c07ee380092e0e9707844d
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-11-04 16:23:39 +0800

    Finishing status section mobile

---
+++ src/components/Status.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function kontolodon() {
+  return (
+    <div className='status'>
+      <div className='role'>
+        <div className='uiux'>UI/UX Designer</div>
+        <div className='website'>Web Developer</div>
+        <div className='pm'>Project Manager</div>
+      </div>
+      <div className='information'>
+        <div>
+          <div className='number'>2</div>
+          <div className='text'>Years Of Experience</div>
+        </div>
+        <div>
+          <div className='number'>15+</div>
+          <div className='text'>Working Projects</div>
+        </div>
+        <div>
+          <div className='number'>2021</div>
+          <div className='text'>Starting Since</div>
+        </div>
+      </div>
+    </div>
+  )
+}
