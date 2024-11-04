dffc666e6daf17d3662c3699396efa2c0f812a07
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-11-04 15:49:03 +0800

    Finishing hero section in mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Hero from './components/Hero'
+import Status from './components/Status'
 
 function App() {
 
   return (
     <>
-      
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import myPicture from '../assets/foto-sic-no-background-part1.png'
+
+export default function Hero() {
+  return (
+    <div className='hero'>
+      <h1 className='title'>Portfolio Design</h1>
+      <div className='year'>2024</div>
+      <div className='wrapper'>
+        <div className='namecard'>
+          <h4>Bisma Bendesa</h4>
+          <p>He/Him</p>
+        </div>
+        <img src={myPicture} alt="mypicture" />
+      </div>
+    </div>
+  )
+}
